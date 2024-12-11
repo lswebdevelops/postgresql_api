cecfb422abe2a0fed022f229dfa929e5ca8da5b1
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-12-11 09:31:55 -0300

    first schema created

--- social-repo/src/test/routes/users.test.js
+++ social-repo/src/test/routes/users.test.js
@@ -1,15 +1,57 @@
-const request = require('supertest');
-const buildApp = require('../../app');
-const UserRepo = require('../../repos/user-repo');
-const pool = require('../../pool');
+const request = require("supertest");
+const buildApp = require("../../app");
+const UserRepo = require("../../repos/user-repo");
+const pool = require("../../pool");
 
-beforeAll(() => {
-  return pool.connect({
-    host: 'localhost',
+const { randomBytes } = require("crypto");
+const { default: migrate } = require("node-pg-migrate");
+const format = require("pg-format");
+
+beforeAll(async () => {
+  // randomly generatyiong a role name to connect to pg as
+  // always starts with an a> because of postgres roles
+  const roleName = "a" + randomBytes(4).toString("hex");
+  // connect to pg as usual
+  await pool.connect({
+    host: "localhost",
     port: 5432,
-    database: 'socialnetwork-test',
-    user: 'postgres',
-    password: '20126619',
+    database: "socialnetwork-test",
+    user: "postgres",
+    password: "20126619",
+  });
+  // create a new role
+  await pool.query(`
+      create role ${roleName} with login password '${roleName}';
+      `);
+  // create a schema with the same name
+  await pool.query(`
+        create schema ${roleName} authorization ${roleName};
+        `);
+  // disconnect entirely from pg
+  await pool.close();
+
+  // run our migrations in the new schema
+  await migrate({
+    schema: roleName,
+    direction: "up",
+    log: () => {},
+    noLock: true,
+    dir: "migrations",
+    databaseUrl: {
+      host: "localhost",
+      port: 5432,
+      database: "socialnetwork-test",
+      user: roleName,
+      password: roleName,
+    },
+  });
+  // connect to pg as the newly created role
+  await pool.connect({
+    host: "localhost",
+    port: 5432,
+    database: "socialnetwork-test",
+    user: roleName,
+    password: roleName,
   });
 });
 
@@ -17,12 +59,12 @@ afterAll(() => {
   return pool.close();
 });
 
-it('create a user', async () => {
+it("create a user", async () => {
   const startingCount = await UserRepo.count();
 
   await request(buildApp())
-    .post('/users')
-    .send({ username: 'testuser', bio: 'test bio' })
+    .post("/users")
+    .send({ username: "testuser", bio: "test bio" })
     .expect(200);
 
   const finishCount = await UserRepo.count();
